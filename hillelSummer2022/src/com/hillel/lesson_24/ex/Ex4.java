c0b981d4fdcec8d1fa0adfa8b0683bcf70f70b83
Oleksand Stepurko <stepurko.alexandr@gmail.com>
2022-08-15 20:10:43 +0200

    Classwork 24

---
+++ hillelSummer2022/src/com/hillel/lesson_24/ex/Ex4.java
@@ -0,0 +1,11 @@
+package com.hillel.lesson_24.ex;
+
+public class Ex4 {
+    public static void main(String[] args) {
+//        int[] x = {120, 200, 016}; // 8
+        int[] x = {120, 0200, 117};
+        for (int j : x) {
+            System.out.print(j + " ");
+        }
+    }
+}
